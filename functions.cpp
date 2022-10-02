a4e8049c781934e76dc634558b19f45e87e008af
KostasEreksonas <k.ereksonas@gmail.com>
2022-10-02 13:14:10 +0300

    Updated spacing

--- functions.cpp
+++ functions.cpp
@@ -11,14 +11,11 @@ bool occupied[3][3]={{false, false, false}, {false, false, false}, {false, false
 int counter = 0;
 
 void welcomeMsg() {
-
 	std::cout << "Welcome to Tic-Tac-Toe game!\n";
 	board();
-
 }
 
 void board() {
-
     std::cout << "    |    |    \n";
     std::cout << "  " << spaces[0][0] << " |  " << spaces[0][1] << " |  " << spaces[0][2] << " \n";
     std::cout << "____|____|____\n";
@@ -28,13 +25,10 @@ void board() {
     std::cout << "    |    |    \n";
     std::cout << "  " << spaces[2][0] << " |  " << spaces[2][1] << " |  " << spaces[2][2] << " \n";
     std::cout << "    |    |    \n";
-
 }
 
 void choice(char player, int space) {
-
 	switch(space) {
-
 		case 1:
 			if (spaces[0][0] == 'X' || spaces[0][0] == 'O') {
 				std::cout << "Space 1 is already occupied, please choose another.\n";
@@ -46,7 +40,6 @@ void choice(char player, int space) {
 				counter++;
 				break;
 			}
-
 		case 2:
 			if (spaces[0][1] == 'X' || spaces[0][1] == 'O') {
 				std::cout << "Space 2 is already occupied, please choose another.\n";
@@ -58,7 +51,6 @@ void choice(char player, int space) {
 				counter++;
 				break;
 			}
-
 		case 3:
 			if (spaces[0][2] == 'X' || spaces[0][2] == 'O') {
 				std::cout << "Space 3 is already occupied, please choose another.\n";
@@ -70,7 +62,6 @@ void choice(char player, int space) {
 				counter++;
 				break;
 			}
-
 		case 4:
 			if (spaces[1][0] == 'X' || spaces[1][0] == 'O') {
 				std::cout << "Space 4 is already occupied, please choose another.\n";
@@ -82,7 +73,6 @@ void choice(char player, int space) {
 				counter++;
 				break;
 			}
-
 		case 5:
 			if (spaces[1][1] == 'X' || spaces[1][1] == 'O') {
 				std::cout << "Space 5 is already occupied, please choose another.\n";
@@ -94,7 +84,6 @@ void choice(char player, int space) {
 				counter++;
 				break;
 			}
-
 		case 6:
 			if (spaces[1][2] == 'X' || spaces[1][2] == 'O') {
 				std::cout << "Space 6 is already occupied, please choose another.\n";
@@ -106,7 +95,6 @@ void choice(char player, int space) {
 				counter++;
 				break;
 			}
-
 		case 7:
 			if (spaces[2][0] == 'X' || spaces[2][0] == 'O') {
 				std::cout << "Space 7 is already occupied, please choose another.\n";
@@ -118,7 +106,6 @@ void choice(char player, int space) {
 				counter++;
 				break;
 			}
-
 		case 8:
 			if (spaces[2][1] == 'X' || spaces[2][1] == 'O') {
 				std::cout << "Space 8 is already occupied, please choose another.\n";
@@ -130,7 +117,6 @@ void choice(char player, int space) {
 				counter++;
 				break;
 			}
-
 		case 9:
 			if (spaces[2][2] == 'X' || spaces[2][2] == 'O') {
 				std::cout << "Space 9 is already occupied, please choose another.\n";
@@ -142,62 +128,39 @@ void choice(char player, int space) {
 				counter++;
 				break;
 			}
-
 		default:
 			std::cout << "Invalid number selected, please try again.\n";
-
 	}
-
 }
 
 bool checkWin() {
-
 	if ((spaces[0][0] == 'X' && spaces[0][1] == 'X' && spaces[0][2] == 'X') || (spaces[1][0] == 'X' && spaces[1][1] == 'X' && spaces[1][2] == 'X') || (spaces[2][0] == 'X' && spaces[2][1] == 'X' && spaces[2][2] == 'X') || (spaces[0][0] == 'X' && spaces[1][0] == 'X' && spaces[2][0] == 'X') || (spaces[0][1] == 'X' && spaces[1][1] == 'X' && spaces[2][1] == 'X') || (spaces[0][2] == 'X' && spaces[1][2] == 'X' && spaces[2][2] == 'X') || (spaces[0][0] == 'X' && spaces[1][1] == 'X' && spaces[2][2] == 'X') || (spaces[2][0] == 'X' && spaces[1][1] == 'X' && spaces[0][2] == 'X')) {
-
 		std::cout << "Player 1 wins!\n";
 		return true;
-
 	}
-
 	if ((spaces[0][0] == 'O' && spaces[0][1] == 'O' && spaces[0][2] == 'O') || (spaces[1][0] == 'O' && spaces[1][1] == 'O' && spaces[1][2] == 'O') || (spaces[2][0] == 'O' && spaces[2][1] == 'O' && spaces[2][2] == 'O') || (spaces[0][0] == 'O' && spaces[1][0] == 'O' && spaces[2][0] == 'O') || (spaces[0][1] == 'O' && spaces[1][1] == 'O' && spaces[2][1] == 'O') || (spaces[0][2] == 'O' && spaces[1][2] == 'O' && spaces[2][2] == 'O') || (spaces[0][0] == 'O' && spaces[1][1] == 'O' && spaces[2][2] == 'O') || (spaces[2][0] == 'O' && spaces[1][1] == 'O' && spaces[0][2] == 'O')) {
-
 		std::cout << "Player 2 wins!\n";
 		return true;
-
 	}
-
 	return false;
-
 }
 
 // Call this after checking for game-winning clauses
 // If the board is full and no clauses for winning were satisfied, declare a tie
 bool checkTie() {
-
 	int c = 0;
-
 	for (int i = 0; i < 3; i++) {
 		for (int j = 0; j < 3; j++) {
-
 			if (occupied[i][j] == true) {
-
 				c++;
-
 			}
-
 		}
 	}
-
 	if (c == 9) {
-
 		std::cout << "It's a tie!\n";
 		return true;
-
 	}
 	else {
-
 		return false;
-
 	}
-
 }
--- tictactoe.cpp
+++ tictactoe.cpp
@@ -6,56 +6,38 @@ bool win=false;
 bool tie=false;
 
 int main() {
-
 	// Define symbols for each player
 	char player1='X';
 	char player2='O';
-
 	// Initialize a variable to store a number of the chosen space
 	int num;
-
 	// Display welcome message
 	welcomeMsg();
-
 	while (win != true || tie != true) {
-
 		if (counter % 2 == 0) {
-
 			std::cout << "Choose a space for player 1: ";
 			std::cin >> num;
-
 			choice(player1, num);
 			board();
-
 			if (win != checkWin()) {
 				break;
 			}
 			if (tie != checkTie()) {
 				break;
 			}
-
 		}
-
 		else {
-
 			std::cout << "Choose a space for player 2: ";
 			std::cin >> num;
-
 			choice(player2, num);
 			board();
-
 			if (win != checkWin()) {
 				break;
 			}
 			if (tie != checkTie()) {
 				break;
 			}
-
 		}
-
-
 	}
-
 	return 0;
-
 }
